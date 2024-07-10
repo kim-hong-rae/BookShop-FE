1a9e19d5e9a2b8f1d65953e8bd922faefab59c9c
kimhongrae <khr0690@hanmail.net>
2024-07-10 15:18:47 +0900

    feat: 초기 설정

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <>
+      <hr />
+      <footer>copyright(c), 2023</footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import { formatNumber } from "../utils/format";
+
+const Home = () => {
+  return (
+    <>
+      <div>Home</div>
+    </>
+  );
+};
+
+export default Home;
