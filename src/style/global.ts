bc4ab7b6f4dc04781b4a694b23ace9e97106d1ae
kimhongrae <khr0690@hanmail.net>
2024-07-10 15:19:00 +0900

    feat : 글로벌 스타일 및 테마 설정

---
+++ src/style/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import "sanitize.css";
+import { ThemeName } from "./theme";
+
+interface Props {
+  themeName: ThemeName;
+}
+
+export const GlobalStyle = createGlobalStyle<Props>`
+  body {
+  margin: 0;
+  padding: 0;
+  }
+  h1 {
+  margin: 0;
+  }
+
+  *{
+   color : ${(props) => (props.themeName === "light" ? "black" : "white")}
+  }
+`;
