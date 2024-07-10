b6323652a2de0fca069050f4a8ab26b2b26e1cb5
kimhongrae <khr0690@hanmail.net>
2024-07-11 02:13:43 +0900

    feat: localstorage에 테마 저장 및 테마 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import { GlobalStyle } from "./style/global";
 import { ThemeProvider } from "styled-components";
-import { dark, light } from "./style/theme";
+import { ThemeName, dark, getTheme, light } from "./style/theme";
+import ThemeSwitcher from "./components/header/ThemeSwitcher";
+import { useContext, useState } from "react";
+import { BookStoreThemeProvider, ThemeContext } from "./context/themeContext";
 
 function App() {
   return (
-    <ThemeProvider theme={light}>
-      <GlobalStyle themeName="light" />
+    <BookStoreThemeProvider>
+      <ThemeSwitcher />
       <Layout>
         <Home />
       </Layout>
-    </ThemeProvider>
+    </BookStoreThemeProvider>
   );
 }
 
