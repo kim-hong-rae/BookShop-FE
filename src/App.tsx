c4cf337ab57d60b61160a104eade3f90782af771
kimhongrae <khr0690@hanmail.net>
2024-07-18 01:33:35 +0900

    feat: 라우터

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import Layout from "./components/layout/Layout";
-import Detail from "./pages/Detail";
 import Home from "./pages/Home";
-import { GlobalStyle } from "./style/global";
-import { ThemeProvider } from "styled-components";
-import { ThemeName, dark, getTheme, light } from "./style/theme";
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
-import { useContext, useState } from "react";
-import { BookStoreThemeProvider, ThemeContext } from "./context/themeContext";
+import { BookStoreThemeProvider } from "./context/themeContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./components/common/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+    errorElement: <Error />,
+  },
+  {
+    path: "/books",
+    element: (
+      <Layout>
+        <div>도서목록</div>
+      </Layout>
+    ),
+  },
+]);
 
 function App() {
   return (
     <BookStoreThemeProvider>
       <ThemeSwitcher />
-      <Layout>
-        <Home />
-      </Layout>
+      <RouterProvider router={router}></RouterProvider>
     </BookStoreThemeProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "sanitize.css";
-import { ThemeContext, state } from "./context/themeContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
