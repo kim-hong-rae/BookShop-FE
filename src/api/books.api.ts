7d2c40410f9456a462da01455a8349ca2944a413
kimhongrae <khr0690@hanmail.net>
2024-07-30 20:44:12 +0900

    feat : book api

---
+++ src/api/books.api.ts
@@ -0,0 +1,37 @@
+import { Book, BookDetail } from "../models/book.model";
+import { Pagination } from "../models/pagination.model";
+import { httpClient } from "./http";
+
+interface FetchBooksParams {
+  category_id?: number;
+  news?: boolean;
+  currenPage?: number;
+  limit: number;
+}
+
+interface FetchBooksResponse {
+  books: Book[];
+  pagination: Pagination;
+}
+
+export const fetchBooks = async (params: FetchBooksParams) => {
+  try {
+    const response = await httpClient.get<FetchBooksResponse>("/books", {
+      params: params,
+    });
+    return response.data;
+  } catch (error) {
+    return {
+      books: [],
+      pagination: {
+        totalCount: 0,
+        currentPage: 1,
+      },
+    };
+  }
+};
+
+export const fetchBook = async (bookId: string) => {
+  const response = await httpClient.get<BookDetail>(`/books/${bookId}`);
+  return response.data;
+};
