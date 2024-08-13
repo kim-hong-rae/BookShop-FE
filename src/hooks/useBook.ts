aa9c6bac10039b4c2503250e5711c53b34bf3154
kimhongrae <khr0690@hanmail.net>
2024-08-13 22:54:41 +0900

    feat : useBook 커스텀 훅

--- src/hooks/useBook.ts
+++ src/hooks/useBook.ts
@@ -1,9 +1,56 @@
 import { useEffect, useState } from "react";
 import { BookDetail } from "../models/book.model";
-import { fetchBook } from "../api/books.api";
+import { fetchBook, likeBook, unlikeBook } from "../api/books.api";
+import { useAuthStore } from "../store/authStore";
+import { useAlert } from "./useAlert";
+import { addCart } from "../api/carts.api";
 
 export const useBook = (bookId: string | undefined) => {
   const [book, setBook] = useState<BookDetail | null>(null);
+  const { isloggedIn } = useAuthStore();
+  const [cartAdded, setCartAdded] = useState(false);
+
+  const {showAlert} = useAlert();
+
+  const likeToggle = () => {
+    //권한 확인
+    if (!isloggedIn) {
+      showAlert("로그인이 필요합니다.");
+    }
+
+    if (!book) return;
+
+    if (book.liked) {
+      //라이크 상태 -> 언라이크 실행
+      unlikeBook(book.id).then(() => {
+        setBook({
+          ...book,
+          liked: false,
+          likes: book.likes - 1,
+        });
+      });
+    } else {
+      //언라이크 상태 -> 라이크 실행
+      likeBook(book.id).then(() => {
+        setBook({
+          ...book,
+          liked: true,
+          likes: book.likes + 1,
+        });
+      });
+    }
+  };
+
+  const addToCart = (quantity: number) => {
+    if (!book) return;
+    addCart({
+      book_id: book.id,
+      quantity: quantity,
+    }).then(() => {
+      setCartAdded(true);
+      setTimeout(() => setCartAdded(false), 3000);
+    });
+  };
 
   useEffect(() => {
     if (!bookId) return;
@@ -11,5 +58,5 @@ export const useBook = (bookId: string | undefined) => {
       setBook(book);
     });
   }, [bookId]);
-  return { book };
+  return { book, likeToggle, addToCart, cartAdded };
 };
