ff9c67a70c33e77dbae7167afc329e902e0a74c9
kimhongrae <khr0690@hanmail.net>
2024-08-13 12:41:23 +0900

    feat: useAlert훅에 confirm 추가

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -4,7 +4,14 @@ export const useAlert = () => {
   const showAlert = useCallback((message: string) => {
     window.alert(message);
   }, []);
-  return showAlert;
+
+  const showConfirm = useCallback((message: string, onConfirm: () => void) => {
+    if (window.confirm(message)) {
+      onConfirm();
+    }
+  }, []);
+
+  return { showAlert, showConfirm };
 };
 
 //useCallback 사용 이유 - 해당 함수가 필요할 때만 다시 생성되게.
