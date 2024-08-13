f49c5b12a61c0fc5ad0ac3e566c8317e5dd848df
kimhongrae <khr0690@hanmail.net>
2024-08-13 22:52:40 +0900

    fix: 버튼 컴포넌트 맞춤법 오류 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { ButtonScheme, ButtonSize } from "../../style/theme";
-import { ButtonHTMLAttributes } from "react";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -24,17 +23,16 @@ const Button = ({ children, size, scheme, disabled, isLoading }: Props) => {
 };
 
 const ButtonStyle = styled.button<Omit<Props, "children">>`
-  font-size : ${({ theme, size }) => theme.button[size].fontSize};
+  font-size: ${({ theme, size }) => theme.button[size].fontSize};
   padding: ${({ theme, size }) => theme.button[size].padding};
   color: ${({ theme, scheme }) => theme.buttonScheme[scheme].color};
   background-color: ${({ theme, scheme }) =>
     theme.buttonScheme[scheme].backgroundColor};
   border: 0;
-  border-radius : ${({ theme }) => theme.borderRadius.default}
-  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)}
-  pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")}
-  cursor:${({ disabled }) => (disabled ? "none" : "pointer")}
-
+  border-radius: ${({ theme }) => theme.borderRadius.default};
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+  pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
+  cursor: ${({ disabled }) => (disabled ? "none" : "pointer")};
 `;
 
 export default Button;
